169d1c25541802fc84d7ae90e0c6c6d8e633d9c7
Nimba Ram <NimbaR@talentica-all.com>
2021-10-12 16:36:02 +0530

    added middleware

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { jwtConstants } from './constants';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 
 @Module({
@@ -13,7 +14,7 @@ import { JwtModule } from '@nestjs/jwt';
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
     })],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 
 })
--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -6,18 +6,20 @@ import {
   ParseUUIDPipe,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import {
   CreateStudentDto,
 } from './dto/student.dto';
 import { StudentService } from './student.service';
 import { StudentsTable } from '../entity/student.entity'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 
 @Controller('student')
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
-
+  @UseGuards(JwtAuthGuard)
   @Get()
   getStudent(): Promise<StudentsTable[]> {
     return this.studentService.getStudents();
